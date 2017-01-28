2e16e066f5bfd1c0259a3013e6782d5ca56409a6
Jorge <jogonzal@microsoft.com>
2017-01-28 15:13:30 -0800

    Add similarity classes

---
+++ Hw3/Similarities/BaseSimilarity.cs
@@ -0,0 +1,9 @@
+﻿using Hw3.SparseModels;
+
+namespace Hw3.Similarities
+{
+	public abstract class BaseSimilarity
+	{
+		public abstract double CalculateSimilarity(Article articleX, Article articleY);
+	}
+}
---
+++ Hw3/Similarities/L2Similarity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Hw3.SparseModels;
+
+namespace Hw3.Similarities
+{
+	public class L2Similarity : BaseSimilarity
+	{
+		public override double CalculateSimilarity(Article articleX, Article articleY)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
